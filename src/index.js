d37c97b86523ba626855cc3939247c62b69dbd49
Łukasz Kozłowski <ryuke12@gmail.com>
2020-08-03 17:45:53 +0200

    Implement redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
 import 'tachyons';
+import * as serviceWorker from './serviceWorker';
+import App from './containers/App';
+import { searchRobots } from './store/reducers';
+import './index.css';
 
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/store/reducers.js
@@ -0,0 +1,14 @@
+import * as actionTypes from './actionTypes';
+
+const initialState = {
+  searchField: '',
+};
+
+export const searchRobots = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case actionTypes.CHANGE_SEARCH_FIELD:
+      return { ...state, searchField: action.payload };
+    default:
+      return state;
+  }
+};
